d1e352809e7aa395dc4fe4fed2d235aff23419e0
Sotos <stilos@gmail.com>
2021-09-24 00:04:01 +0300

    Rating and product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,29 @@
-import data from './data.js'
+import Product from "./components/Product.js";
+import data from "./data.js";
 
 function App() {
   return (
     <div className="grid-container">
-            <header className="row">
-                <div>
-                    <a className="brand" href="index.html">Salvo Ltd</a>
-                </div>
-                <div>
-                    <a href="cart.html">Cart</a>
-                    <a href="signing.html">Sign In</a>
-                </div>
-            </header>
-            <main>
-                <div className="row center">
-                  {
-                    data.products.map((product) => (
-                      <div key={product._id} className="card">
-                        <a href={`/product/${product._id}`}>
-                           <img className="medium" 
-                           src={product.image} 
-                           alt={product.name} 
-                           />
-                        </a>
-                        <div className="card-body">
-                          <a href={`/product/${product._id}`}>
-                            <h2>{product.name}</h2>
-                          </a>
-                          <div className="rating">
-                              <span> <i className="fa fa-star"></i> 
-                              </span>
-                              <span> <i className="fa fa-star"></i> 
-                              </span>    
-                              <span> <i className="fa fa-star"></i>
-                              </span>
-                              <span> <i className="fa fa-star"></i>
-                              </span>
-                              <span> <i className="fa fa-star"></i>
-                              </span>
-                              </div>
-                            <div className="price">${product.price}</div>
-                        </div>
-                      </div>
-                    ))
-                  }
-                </div>
-            </main>
-            <footer className="row center">All rights reserved</footer>
-      </div>
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">
+            Salvo Ltd
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signing.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <Product key={product._id} product={product}></Product>
+          ))}
+        </div>
+      </main>
+      <footer className="row center">All rights reserved</footer>
+    </div>
   );
 }
 
