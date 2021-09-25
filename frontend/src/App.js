826becbb16dbeae41d731b2eb682930f1d1aedaf
Sotos <stilos@gmail.com>
2021-09-25 14:51:39 +0300

    Create a product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import Product from "./components/Product.js";
-import data from "./data.js";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="index.html">
-            Salvo Ltd
-          </a>
-        </div>
-        <div>
-          <a href="cart.html">Cart</a>
-          <a href="signing.html">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product}></Product>
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All rights reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="index.html">
+              Salvo Ltd
+            </a>
+          </div>
+          <div>
+            <a href="cart.html">Cart</a>
+            <a href="signing.html">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All rights reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
